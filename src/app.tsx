0840ffb188fdc2845b4f88ede14291a59c3da267
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-03-28 23:25:59 +0100

    feat(core): implement boilerplate static footer

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,14 @@
+import { Footer } from './layouts/footer/Footer';
+import { Header } from './layouts/header/Header';
+
+// TODO: Use context for light / dark mode
+
 export function App() {
-    return <h1>React root setup perfectly !</h1>;
+    return (
+        <>
+            <Header />
+            <h1>React root setup perfectly !</h1>
+            <Footer />
+        </>
+    );
 }
