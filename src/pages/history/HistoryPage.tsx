3c5cd7a1ce5cbcff82609af9f654293ad1042a51
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-03-29 17:46:47 +0100

    refactor(core): implement transaction interface

--- src/pages/history/HistoryPage.tsx
+++ src/pages/history/HistoryPage.tsx
@@ -2,19 +2,34 @@ import { useEffect, useState } from 'react';
 import { TransactionCard } from '../../components/transaction-card/TransactionCard';
 import { TransactionForm } from '../../components/transaction-form/TransactionForm';
 
+interface Transaction {
+    id: number;
+    name: string;
+    cost: number;
+    type: string;
+    category: string;
+}
+
 export function HistoryPage() {
     // TODO: Display Card automatically based on user data.
     // => Map the data to JSX elements
 
-    const [transaction, setTransaction] = useState('');
+    const [transaction, setTransaction] = useState<Transaction | null>(null);
 
     useEffect(() => {
-        if (transaction !== '') {
+        // For testing purposes.
+        if (!transaction) {
+            localStorage.removeItem('transactions');
+        }
+
+        if (transaction) {
+            const stringifiedTransaction = JSON.stringify(transaction);
+
             const response = localStorage.getItem('transactions');
 
             const existingTransactions = JSON.parse(response || '[]');
 
-            const updatedTransaction = [...existingTransactions, transaction];
+            const updatedTransaction = [...existingTransactions, stringifiedTransaction];
 
             localStorage.setItem('transactions', JSON.stringify(updatedTransaction));
         }
@@ -24,25 +39,61 @@ export function HistoryPage() {
         // IDEA: Add some form validation even if it's client side.
 
         const name = formData.get('transaction-name');
+        if (name === null) {
+            throw new Error('Form transaction name should not be null !');
+        }
+
         const cost = formData.get('transaction-cost');
+        if (cost === null) {
+            throw new Error('Form transaction cost should not be null !');
+        }
+
         const type = formData.get('transaction-type');
+        if (type === null) {
+            throw new Error('Form transaction type should not be null !');
+        }
+
         const category = formData.get('transaction-category');
+        if (category === null) {
+            throw new Error('Form transaction category should not be null !');
+        }
+
+        const requiredName = name as string;
+        const requiredCost = parseInt(cost as string);
+        const requiredType = type as string;
+        const requiredCategory = category as string;
 
         // IDEA: Implement interface for Transaction
-        const transaction = {
+        const transaction: Transaction = {
             id: Date.now(),
-            name: name,
-            cost: cost,
-            type: type,
-            category: category,
+            name: requiredName,
+            cost: requiredCost,
+            type: requiredType,
+            category: requiredCategory,
             // TODO: Allow user to choose "when" the transaction has been done.
             // E.g: "date: date"
         };
 
-        const stringTransaction = JSON.stringify(transaction);
+        // const stringTransaction = JSON.stringify(transaction);
 
-        setTransaction(stringTransaction);
+        // setTransaction(stringTransaction);
+
+        setTransaction(transaction);
     }
+
+    // function displayTransaction() {
+    //     const response = localStorage.getItem('transactions');
+
+    //     if (response === null) {
+    //         return <p>No transactions registered for now.</p>;
+    //     }
+
+    //     const existingTransactions = JSON.parse(response);
+
+    //     const transactions = existingTransactions.forEach((transaction) => {
+    //         return <strong key={transaction.id}>one transaction</strong>;
+    //     });
+    // }
 
     return (
         <>
